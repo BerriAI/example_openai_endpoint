a7d1b7d5afcce6441b56c31787e145bf5d565af4
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-10-04 16:34:21 +0530

    fix load testing

--- main.py
+++ main.py
@@ -20,7 +20,11 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 
-limiter = Limiter(key_func=get_remote_address)
+
+def get_request_url(request: Request):
+    return str(request.url)
+
+limiter = Limiter(key_func=get_request_url)
 load_dotenv()
 
 app = FastAPI()
